703a3ed12a2e5a6284518bf3ace62bc5137f8ac7
AnirudhG07 <anirudhgupta@iisc.ac.in>
2025-05-06 15:20:45 +0530

    improve output from cmd_manager

---
+++ src/commands/help.rs
@@ -0,0 +1,11 @@
+pub fn help() -> String {
+    let help_text = r#"
+    Welcome to DBD Deemak Help. You can use the following commands:
+
+    - echo <message>: Echoes the message back to you.
+    - help: Displays this help message.
+    - exit: Exits the program.
+    - clear: Clears the screen.
+    "#;
+    help_text.to_string()
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,5 +1,8 @@
 mod cmds;
-pub use cmds::cmd_manager;
+pub use cmds::{CommandResult, cmd_manager};
 
 mod echo;
 pub use echo::echo;
+
+mod help;
+pub use help::help;
