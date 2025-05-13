4eb9b41ba756e082b9370629bfdd1dd5b93e0c4a
Aditya Thakkar <adityaat@iisc.ac.in>
2025-05-13 12:36:54 +0530

    Added the cursor, text-wrap, and changed font

--- src/screen.rs
+++ src/screen.rs
@@ -3,17 +3,17 @@ use commands::CommandResult;
 use deemak::commands;
 use deemak::utils;
 use raylib::prelude::*;
-use std::path::PathBuf;
-use raylib::ffi::SetConfigFlags;
-use rocket::yansi::Paint;
+use std::{path::PathBuf, mem::take, process::exit, os::raw::c_int};
+use std::ffi::CString;
+use std::os::raw::c_char;
+use raylib::ffi::{DrawTextEx, LoadFontEx, MeasureTextEx, SetConfigFlags, Vector2, ColorFromHSV, DrawRectangle};
+use textwrap::wrap;
 
-// TODO: Wrap text to new lines,
-// TODO: Make Deemak resizeable,
-// TODO: Make the font size relative to the device.
-// TODO: Change the font completely.
-// TODO: Add a cursor (partially implemented)
-// TODO: Add a ascii output
-// TODO: Add a image output
+// BUGGY: Wrap text to new lines,
+// DONE: Make Deemak resizeable,
+// DONE: Make the font size relative to the device.
+// DONE: Change the font completely.
+// BUGGY: Add a cursor
 
 pub struct ShellScreen {
     rl: RaylibHandle,
@@ -23,16 +23,21 @@ pub struct ShellScreen {
     current_dir: PathBuf,
     root_dir: PathBuf,
     cursor_position: Vec<i32>,
+    font: ffi::Font,
+    grid_width: f32,
+    window_width: i32,
 }
 
+pub const FONT_SIZE: f32 = 20.0;
+
 pub const DEEMAK_BANNER: &str = r#"
- _____                            _    
-|  __ \                          | |   
+ _____                            _
+|  __ \                          | |
 | |  | | ___  ___ _ __ ___   __ _| | __
 | |  | |/ _ \/ _ \ '_ ` _ \ / _` | |/ /
-| |__| |  __/  __/ | | | | | (_| |   < 
+| |__| |  __/  __/ | | | | | (_| |   <
 |_____/ \___|\___|_| |_| |_|\__,_|_|\_\
-                                       
+
 Developed by Databased Club, Indian Institute of Science, Bangalore.
 Official Github Repo: https://github.com/databasedIISc/deemak
 "#;
@@ -41,12 +46,37 @@ pub const INITIAL_MSG: &str = "Type commands and press Enter. Try `help` for mor
 
 impl ShellScreen {
     pub fn new() -> Self {
-        let (rl, thread) = raylib::init()
+        unsafe {
+            SetConfigFlags(4);
+        }
+        // Loading Font
+        let font = unsafe {
+            let path = CString::new("JetBrainsMono-2/fonts/ttf/JetBrainsMonoNL-Medium.ttf").unwrap();
+            LoadFontEx(
+                path.as_ptr() as *const c_char,
+                FONT_SIZE as c_int,
+                0 as *mut c_int,
+                0
+            )
+        };
+
+        let grid_width = unsafe {
+            let a_char = CString::new("A").unwrap();
+            MeasureTextEx(
+                font,
+                a_char.as_ptr() as *const c_char,
+                FONT_SIZE,
+                0.0
+            ).x
+        };
+
+        let (rl, thread) = init()
             .size(800, 600)
             .title("DBD Deemak Shell")
             .build();
 
-        unsafe { SetConfigFlags(4) };
+        let window_width = rl.get_screen_width();
+
         let root_dir = utils::find_home().expect("Sekai root directory not found");
 
         Self {
@@ -58,6 +88,9 @@ impl ShellScreen {
             root_dir: root_dir.clone(),
             current_dir: root_dir, // Both point to same path initially
             cursor_position: vec![0, 0],
+            font,
+            grid_width,
+            window_width,
         }
     }
 
@@ -69,7 +102,7 @@ impl ShellScreen {
         // Handle keyboard input
         match self.rl.get_key_pressed() {
             Some(KeyboardKey::KEY_ENTER) => {
-                let input = std::mem::take(&mut self.input_buffer);
+                let input = take(&mut self.input_buffer);
                 self.process_input(&input);
             }
             Some(KeyboardKey::KEY_BACKSPACE) => {
@@ -88,6 +121,11 @@ impl ShellScreen {
             }
             None => {}
         }
+
+        // Handle window re-size
+        if self.rl.is_window_resized() {
+            self.window_width = self.rl.get_screen_width();
+        }
     }
 
     pub fn draw(&mut self) {
@@ -96,34 +134,82 @@ impl ShellScreen {
         d.clear_background(Color::BLACK);
 
         // Draw output lines
+        let mut extra_lines = 0;
         for (i, line) in self.output_lines.iter().enumerate() {
-            d.draw_text(line, 10, 10 + (i as i32 * 20), 20, Color::WHITE);
+            let limit = (self.window_width / self.grid_width as i32) - 5;
+            if line.len() as i32 > limit {
+                let lines = wrap(line, limit as usize);
+                for wrapped_line in lines {
+                    unsafe {
+                        let pos: Vector2 = Vector2{x: 10.0, y: 10.0 + ((i+extra_lines) as f32 * FONT_SIZE)};
+                        let content = CString::new(wrapped_line.to_string()).unwrap();
+                        DrawTextEx(
+                            self.font,
+                            content.as_ptr() as *const c_char,
+                            pos,
+                            FONT_SIZE,
+                            1.0,
+                            ColorFromHSV(0.0, 0.0, 1.0)
+                        );
+                    }
+                    extra_lines += 1;
+                }
+            } else {
+                unsafe {
+                    let pos: Vector2 = Vector2{x: 10.0, y: 10.0 + ((i+extra_lines) as f32 * FONT_SIZE)};
+                    let content = CString::new(line.as_str()).unwrap();
+                    DrawTextEx(
+                        self.font,
+                        content.as_ptr() as *const c_char,
+                        pos,
+                        FONT_SIZE,
+                        1.0,
+                        ColorFromHSV(0.0, 0.0, 1.0)
+                    );
+                }
+            }
         }
 
-        // Draw input prompt and buffer
-        d.draw_text(
-            "> ",
-            10,
-            10 + (self.output_lines.len() as i32 * 20),
-            20,
-            Color::GREEN,
-        );
-        d.draw_text(
-            &self.input_buffer,
-            30,
-            10 + (self.output_lines.len() as i32 * 20),
-            20,
-            Color::WHITE,
-        );
-        self.cursor_position[0] = 30 + (self.input_buffer.len() as i32 * 10);
-        self.cursor_position[1] = 10 + (self.output_lines.len() as i32 * 20);
-        d.draw_rectangle(
-            self.cursor_position[0],
-            self.cursor_position[1],
-            10,
-            20,
-            Color::BLUE,
-        );
+        // '>' at the beginning of every line
+        unsafe {
+            let pos: Vector2 = Vector2{x: 10.0, y: 10.0 + ((self.output_lines.len()+extra_lines) as f32 * FONT_SIZE)};
+            let content = CString::new(">").unwrap();
+            DrawTextEx(
+                self.font,
+                content.as_ptr() as *const c_char,
+                pos,
+                FONT_SIZE,
+                1.0,
+                ColorFromHSV(0.0, 0.0, 1.0)
+            );
+        }
+
+        // Input
+        for (i, char) in self.input_buffer.as_str().chars().enumerate() {
+            unsafe {
+                let pos: Vector2 = Vector2{x: 30.0 + (i as f32 * (2.5 + self.grid_width)), y: 10.0 + ((self.output_lines.len()+extra_lines) as f32 * FONT_SIZE)};
+                let content = CString::new(char.to_string()).unwrap();
+                DrawTextEx(
+                    self.font,
+                    content.as_ptr() as *const c_char,
+                    pos,
+                    FONT_SIZE,
+                    1.0,
+                    ColorFromHSV(0.0, 0.0, 1.0)
+                );
+            }
+        }
+
+        // CURSOR
+        unsafe {
+            DrawRectangle(
+                (30.0 + (self.input_buffer.len() as f32 * (2.5 + self.grid_width))) as c_int,
+                (10.0 + ((self.output_lines.len()+extra_lines) as f32 * FONT_SIZE)) as c_int,
+                self.grid_width as c_int,
+                (self.grid_width*2.5) as c_int,
+                ColorFromHSV(10.0, 10.0, 1.0),
+            );
+        }
     }
 
     pub fn process_input(&mut self, input: &str) {
@@ -151,7 +237,7 @@ impl ShellScreen {
                 self.output_lines.push(INITIAL_MSG.to_string());
             }
             CommandResult::Exit => {
-                std::process::exit(1);
+                exit(1);
             }
             CommandResult::NotFound => {
                 self.output_lines
