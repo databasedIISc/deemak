77e07b1bba3bf039d39bf7f4a4ab9e6edb321290
AnirudhG07 <anirudhgupta@iisc.ac.in>
2025-06-05 14:24:05 +0530

    save functionality done

--- src/commands/save.rs
+++ src/commands/save.rs
@@ -25,7 +25,8 @@ pub fn save(args: &[&str], root_path: &PathBuf) -> String {
                 log::log_info("restore", err_msg.as_str());
                 return err_msg;
             }
-            if parser.has_flag("restore") || parser.has_flag("r") {
+            if parser.has_flag("--restore") || parser.has_flag("-r") {
+                log::log_info("save", "RESTORE PARSED");
                 // Restore operation
                 if can_save(root_path) {
                     // `save_me` file already exists.
@@ -36,7 +37,7 @@ pub fn save(args: &[&str], root_path: &PathBuf) -> String {
                         log::log_error("save", err_msg.as_str());
                         return err_msg;
                     }
-                    "Sekai restored successfully from previous checkpoint.\n".to_string()
+                    "Sekai restored successfully from previously saved progress.\n".to_string()
                 } else {
                     (err_msg + "No restore file found. Please save your progress first with `save` command.")
                         .to_string()
@@ -48,7 +49,7 @@ pub fn save(args: &[&str], root_path: &PathBuf) -> String {
                     log::log_error("save", err_msg.as_str());
                     return err_msg;
                 }
-                "Sekai saved successfully from previous checkpoint.\n".to_string()
+                "Sekai saved successfully from previously saved progress.\n".to_string()
             }
         }
         Err(e) => match &e[..] {
