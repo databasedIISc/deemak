92db2c43591d00eee896bb798f07a53e4289046d
Palak Raisinghani <palakr@Palaks-MacBook-Pro.local>
2025-07-12 14:12:55 +0530

    smthng
    smthng more

--- src/commands/unlock.rs
+++ src/commands/unlock.rs
@@ -3,7 +3,9 @@ use argon2::password_hash::SaltString;
 use super::argparser::ArgParser;
 use super::cmds::{check_dir_info, normalize_path};
 use super::display_relative_path;
-use crate::metainfo::info_reader::add_obj_to_info;
+use crate::metainfo::info_reader::{add_obj_to_info};
+use crate::metainfo::lock_perm::{operation_locked_perm,operation_locked_perm};
+use crate::metainfo::read_lock_perm;
 use crate::metainfo::valid_sekai::create_dir_info;
 use crate::rns::security::{argonhash, characterise_enc_key, decrypt, encrypt};
 use crate::utils::globals::USER_ID;
@@ -41,15 +43,58 @@ pub fn unlock(args: &[&str], current_dir: &PathBuf, root_dir: &Path) -> String {
             if pos_args.len() != 1 {
                 err_msg += "Exactly one positional argument -giving path to directory/file to be unlocked -is expected.";
                 log::log_info("unlock", err_msg.as_str());
+                return err_msg;}         
+            //now we know only 1 argument is there
+            //validate path existence
+            let target = normalize_path(&pos_args[0]);
+            if !target.exists() {
+                err_msg += "Invalid path given";
+                log::log_info("unlock", err_msg.as_str());
                 return err_msg;
             }
-            //now we know only 1 argument is there
+            if let Err(e) = target
+                .file_name()
+                .and_then(|s| s.to_str())
+                .ok_or("Invalid object name"){}
+            //validated path. now check if it is accessible
+            if let Err(msg)=operation_locked_perm(target.parent().unwrap(), "unlock", "you cannot try to unlock a chest/level nested inside a locked directory/level"){
+                err_msg+=msg.as_str();
+                log::log_info("unlock", err_msg.as_str());
+                return err_msg;
+            }
+            //now check if it is a protected thing
+            if let Ok((is_level, is_locked)) = read_lock_perm(&target) {
+                if !is_locked {
+                    err_msg += "This is not locked or already unlocked. Cannot unlock. you can try accessing it directly.";
+                    log::log_info("unlock", err_msg.as_str());
+                    return err_msg;
+                }
+                //it is a lock but unlockable take flag 
+                //ask for flag------------FILLIN-------------
+                let mut user_flag = String::from("user_flag_placeholder");
+                if is_level{
+                    //is level
+                }
+                else {
+                    //is chest 
+                }
+            }
+            }
+            Err(e) => {
+                err_msg += &format!("Failed to parse arguments: {}", e);
+                log::log_error("unlock", err_msg.as_str());
+                return err_msg;
+            }
+        }
+
+            
     //velidity of path
     //test for operation_lock_perm except last one file
     //if permitted then ask for flag
     //take flag and check
     //return message
-}
+    }
+
 fn check_flag(user_flag: String, current_dir: &PathBuf, root_dir: &PathBuf) -> String {
     let LEVELNAME = "worldwar2";
     let LEVEL_ID = "level_tokyo";
--- src/metainfo/mod.rs
+++ src/metainfo/mod.rs
@@ -5,4 +5,4 @@ pub use info_reader::{add_obj_to_info, read_get_obj_info, read_validate_info,
 pub mod valid_sekai;
 
 pub mod lock_perm;
-pub use lock_perm::read_lock_perm;
+pub use lock_perm::{read_lock_perm,operation_locked_perm};
